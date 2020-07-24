41542211634bc3a9c06204538dd18fc43738ac3c
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-07-25 00:15:37 +0700

    Update grad_test.go

--- grad_test.go
+++ grad_test.go
@@ -8,11 +8,11 @@ func TestGrad(t *testing.T) {
     grad, _ := NewGradient().Build()
     colors := grad.Colors(2)
 
-    if len(colors) != 3 {
+    if len(colors) != 2 {
         t.Errorf("%v", len(colors))
     }
 
-    if colors[0].Hex() != "#000001" {
+    if colors[0].Hex() != "#000000" {
         t.Errorf("%v", colors[0].Hex())
     }
 
