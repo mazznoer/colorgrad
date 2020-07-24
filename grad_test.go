31491270d7ef3811f7fd2baa9bfd4c182bb59bba
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-07-25 00:55:02 +0700

    Update grad_test.go

--- grad_test.go
+++ grad_test.go
@@ -1,6 +1,7 @@
 package colorgrad
 
 import (
+    "image/color"
     "testing"
 )
 
@@ -17,6 +18,31 @@ func TestGrad(t *testing.T) {
     }
 
     if colors[1].Hex() != "#ffffff" {
+        t.Errorf("%v", colors[1].Hex())
+    }
+
+    grad, _ = NewGradient().
+        Colors(
+            color.RGBA{255,0,0,255},
+            color.RGBA{255,255,0,255},
+            color.RGBA{0,0,255,255},
+        ).
+        Build()
+    colors = grad.Colors(3)
+
+    if len(colors) != 3 {
+        t.Errorf("%v", len(colors))
+    }
+
+    if colors[0].Hex() != "#ff0000" {
+        t.Errorf("%v", colors[0].Hex())
+    }
+
+    if colors[1].Hex() != "#ffff00" {
+        t.Errorf("%v", colors[0].Hex())
+    }
+
+    if colors[2].Hex() != "#0000ff" {
         t.Errorf("%v", colors[1].Hex())
     }
 }
