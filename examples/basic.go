97fe579fd8b4b3efc155fedc20893e6670e1a4bc
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-07-24 23:25:10 +0700

    Create basic.go (#1)

---
+++ examples/basic.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "image"
+    "image/png"
+    "log"
+    "os"
+    
+    "github.com/mazznoer/colorgrad"
+)
+
+func main() {
+    grad := colorgrad.NewGradient().Build()
+    w := 800
+    h := 90
+    fw := float64(w)
+
+    img := image.NewRGBA(image.Rect(0, 0, w, h))
+        
+    for x := 0; x < w; x++ {
+        col := grad.At(float64(x) / fw)
+        
+        for y := 0; y < h; y++ {
+            img.Set(x, y, col)
+        }
+    }
+    file, err := os.Create("gradient.png")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+    
+    png.Encode(file, img)
+}
