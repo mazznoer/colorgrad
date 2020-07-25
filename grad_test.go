b732388ff8ab549476c515f59c8c15940a90186a
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-07-25 09:43:19 +0700

    Update grad_test.go

--- grad_test.go
+++ grad_test.go
@@ -26,7 +26,7 @@ func TestBasic(t *testing.T) {
     colors = grad.Colors(3)
 
     if len(colors) != 3 {
-        t.Errorf("%v", len(colors))
+        t.Errorf("Expected 3, got %v", len(colors))
     }
     testStr(t, colors[0].Hex(), "#ff0000")
     testStr(t, colors[1].Hex(), "#ffff00")
@@ -35,22 +35,28 @@ func TestBasic(t *testing.T) {
 
 func TestDomain(t *testing.T) {
     grad, _ := NewGradient().
-        HexColors("#000000", "#ff0000", "#ffffff").
+        HexColors("#00ff00", "#ff0000", "#ffff00").
         Domain(0, 0.75, 1).
         Build()
     
-    testStr(t, grad.At(0).Hex(), "#000000")
+    testStr(t, grad.At(0).Hex(), "#00ff00")
     testStr(t, grad.At(0.75).Hex(), "#ff0000")
-    testStr(t, grad.At(1).Hex(), "#ffffff")
-    
+    testStr(t, grad.At(1).Hex(), "#ffff00")
+
+    testStr(t, grad.At(-1).Hex(), "#00ff00")
+    testStr(t, grad.At(1.5).Hex(), "#ffff00")
+
     grad, _ = NewGradient().
-        HexColors("#000000", "#ff0000", "#ffffff").
+        HexColors("#00ff00", "#ff0000", "#ffff00").
         Domain(15, 25, 63).
         Build()
     
-    testStr(t, grad.At(15).Hex(), "#000000")
+    testStr(t, grad.At(15).Hex(), "#00ff00")
     testStr(t, grad.At(25).Hex(), "#ff0000")
-    testStr(t, grad.At(63).Hex(), "#ffffff")
+    testStr(t, grad.At(63).Hex(), "#ffff00")
+
+    testStr(t, grad.At(10).Hex(), "#00ff00")
+    testStr(t, grad.At(67).Hex(), "#ffff00")
 }
 
 func testStr(t *testing.T, result, expected string) {
