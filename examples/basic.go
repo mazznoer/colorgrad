01a24d37c71912a981e435c5a05a08c91bccf9ef
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-07-27 14:31:16 +0700

    Tidy up (#11)
    * Update grad_test.go
    
    * Update basic.go

--- examples/basic.go
+++ examples/basic.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "image"
-    "image/png"
-    "os"
-    
-    "github.com/mazznoer/colorgrad"
+	"image"
+	"image/png"
+	"os"
+
+	"github.com/mazznoer/colorgrad"
 )
 
 func main() {
-    grad := colorgrad.NewGradient().Build()
-    w := 800
-    h := 90
-    fw := float64(w)
+	grad, _ := colorgrad.NewGradient().Build()
+	w := 800
+	h := 90
+	fw := float64(w)
 
-    img := image.NewRGBA(image.Rect(0, 0, w, h))
-        
-    for x := 0; x < w; x++ {
-        col := grad.At(float64(x) / fw)
-        
-        for y := 0; y < h; y++ {
-            img.Set(x, y, col)
-        }
-    }
+	img := image.NewRGBA(image.Rect(0, 0, w, h))
 
-    file, err := os.Create("gradient.png")
+	for x := 0; x < w; x++ {
+		col := grad.At(float64(x) / fw).Clamped()
 
-    if err != nil {
-        panic(err.Error())
-    }
-    defer file.Close()
-    
-    png.Encode(file, img)
+		for y := 0; y < h; y++ {
+			img.Set(x, y, col)
+		}
+	}
+
+	file, err := os.Create("gradient.png")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+
+	png.Encode(file, img)
 }
