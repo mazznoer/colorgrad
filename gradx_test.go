295ce08fada65d9c796470f2fb526b0a828aaab8
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-07-29 15:18:07 +0700

    Update gradx_test.go

--- gradx_test.go
+++ gradx_test.go
@@ -11,12 +11,11 @@ func TestX(t *testing.T) {
 	testStr(t, grad2.At(0).Hex(), "#000000")
 	testStr(t, grad2.At(1).Hex(), "#ffffff")
 
-	colors1 := grad.Colors(5)
-	colors2 := IntoColors(colors1)
+	colors1 := grad.Colors(5)      // []colorful.Color
+	colors2 := IntoColors(colors1) // []color.Color
 
-	for i, c1 := range colors1 {
-		var c2 color.Color
-		c2 = colors2[i]
+	for i, c2 := range colors2 {
+		var c1 color.Color = colors1[i]
 		if c1 != c2 {
 			t.Errorf("%v != %v", c1, c2)
 		}
