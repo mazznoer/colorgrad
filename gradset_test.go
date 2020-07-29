795ad00b11bff43dcb08f3d7257b3f5a41098788
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-07-29 14:38:40 +0700

    Create gradset_test.go

---
+++ gradset_test.go
@@ -0,0 +1,32 @@
+package colorgrad
+
+import (
+	"testing"
+)
+
+func TestPreset(t *testing.T) {
+	testGrad(t, Inferno(), "#000004", "#fcffa4")
+	testGrad(t, Magma(), "#000004", "#fcfdbf")
+	testGrad(t, Plasma(), "#0d0887", "#f0f921")
+	testGrad(t, Spectral(), "#9e0142", "#5e4fa2")
+	testGrad(t, Viridis(), "#440154", "#fee825")
+
+	testGrad(t, Blues(), "#f7fbff", "#08306b")
+	testGrad(t, Greens(), "#f7fcf5", "#00441b")
+	testGrad(t, Greys(), "#ffffff", "#000000")
+	testGrad(t, Oranges(), "#fff5eb", "#7f2704")
+	testGrad(t, Purples(), "#fcfbfd", "#3f007d")
+	testGrad(t, Reds(), "#fff5f0", "#67000d")
+}
+
+func testGrad(t *testing.T, grad Gradient, start, end string) {
+	a := grad.At(0).Hex()
+	if a != start {
+		t.Errorf("Expected %v, got %v", start, a)
+	}
+
+	b := grad.At(1).Hex()
+	if b != end {
+		t.Errorf("Expected %v, got %v", end, b)
+	}
+}
