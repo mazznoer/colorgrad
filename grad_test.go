cf56bd4a5e507d42cf56ae442a8291bcdf3de24b
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-08-07 22:04:07 +0700

    Update grad_test.go

--- grad_test.go
+++ grad_test.go
@@ -152,6 +152,37 @@ func TestDomain(t *testing.T) {
 	testStr(t, grad.At(67).Hex(), "#ffff00")
 }
 
+func TestSharp(t *testing.T) {
+	grad, _ := NewGradient().Build()
+	grad2 := grad.Sharp(7)
+	testStr(t, grad2.At(0).Hex(), "#000000")
+	testStr(t, grad2.At(1).Hex(), "#ffffff")
+
+	testStr(t, grad2.At(math.NaN()).Hex(), "#000000")
+	testStr(t, grad2.At(-0.01).Hex(), "#000000")
+	testStr(t, grad2.At(1.01).Hex(), "#ffffff")
+
+	colors := grad2.Colors(7)
+	if len(colors) != 7 {
+		t.Errorf("Expected 7, got %v", len(colors))
+	}
+	testStr(t, colors[0].Hex(), "#000000")
+	testStr(t, colors[6].Hex(), "#ffffff")
+}
+
+func TestGetColors(t *testing.T) {
+	grad, _ := NewGradient().Build()
+	colors1 := grad.Colors(5)  // []colorful.Color
+	colors2 := grad.Colors2(5) // []color.Color
+
+	for i, c2 := range colors2 {
+		var c1 color.Color = colors1[i]
+		if c1 != c2 {
+			t.Errorf("%v != %v", c1, c2)
+		}
+	}
+}
+
 func testStr(t *testing.T, result, expected string) {
 	if result != expected {
 		t.Errorf("Expected %v, got %v", expected, result)
