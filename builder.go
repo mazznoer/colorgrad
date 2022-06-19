dedda26718db6b1d2ff70fd2c3979b69c646338e
Nor Khasyatillah <mazznoer@ymail.com>
2022-06-19 19:38:14 +0700

    move GradientBuilder to separate file

---
+++ builder.go
@@ -0,0 +1,106 @@
+package colorgrad
+
+import (
+	"fmt"
+	"image/color"
+
+	"github.com/lucasb-eyer/go-colorful"
+	"github.com/mazznoer/csscolorparser"
+)
+
+type GradientBuilder struct {
+	colors            []colorful.Color
+	pos               []float64
+	mode              BlendMode
+	interpolation     Interpolation
+	invalidHtmlColors []string
+}
+
+func NewGradient() *GradientBuilder {
+	return &GradientBuilder{
+		mode:          BlendRgb,
+		interpolation: InterpolationLinear,
+	}
+}
+
+func (gb *GradientBuilder) Colors(colors ...color.Color) *GradientBuilder {
+	for _, c := range colors {
+		col, _ := colorful.MakeColor(c)
+		gb.colors = append(gb.colors, col)
+	}
+	return gb
+}
+
+func (gb *GradientBuilder) HtmlColors(htmlColors ...string) *GradientBuilder {
+	for _, s := range htmlColors {
+		c, err := csscolorparser.Parse(s)
+		if err != nil {
+			gb.invalidHtmlColors = append(gb.invalidHtmlColors, s)
+			continue
+		}
+		gb.colors = append(gb.colors, colorful.Color{R: c.R, G: c.G, B: c.B})
+	}
+	return gb
+}
+
+func (gb *GradientBuilder) Domain(domain ...float64) *GradientBuilder {
+	gb.pos = domain
+	return gb
+}
+
+func (gb *GradientBuilder) Mode(mode BlendMode) *GradientBuilder {
+	gb.mode = mode
+	return gb
+}
+
+func (gb *GradientBuilder) Interpolation(mode Interpolation) *GradientBuilder {
+	gb.interpolation = mode
+	return gb
+}
+
+func (gb *GradientBuilder) Build() (Gradient, error) {
+	zgrad := Gradient{
+		grad: zeroGradient{},
+		dmin: 0,
+		dmax: 1,
+	}
+
+	if gb.invalidHtmlColors != nil {
+		return zgrad, fmt.Errorf("Invalid HTML colors: %q", gb.invalidHtmlColors)
+	}
+
+	if len(gb.colors) == 0 {
+		// Default colors
+		gb.colors = []colorful.Color{
+			{R: 0, G: 0, B: 0}, // black
+			{R: 1, G: 1, B: 1}, // white
+		}
+	} else if len(gb.colors) == 1 {
+		gb.colors = append(gb.colors, gb.colors[0])
+	}
+
+	var pos []float64
+
+	if len(gb.pos) == 0 {
+		pos = linspace(0, 1, uint(len(gb.colors)))
+	} else if len(gb.pos) == len(gb.colors) {
+		for i := 0; i < len(gb.pos)-1; i++ {
+			if gb.pos[i] > gb.pos[i+1] {
+				return zgrad, fmt.Errorf("Domain number %v (%v) is bigger than the next domain (%v)", i+1, gb.pos[i], gb.pos[i+1])
+			}
+		}
+		pos = gb.pos
+	} else if len(gb.pos) == 2 {
+		if gb.pos[0] >= gb.pos[1] {
+			return zgrad, fmt.Errorf("Wrong domain.")
+		}
+		pos = linspace(gb.pos[0], gb.pos[1], uint(len(gb.colors)))
+	} else {
+		return zgrad, fmt.Errorf("Wrong domain.")
+	}
+
+	if gb.interpolation == InterpolationLinear {
+		return newLinearGradient(gb.colors, pos, gb.mode), nil
+	}
+	return newSplineGradient(gb.colors, pos, gb.mode, gb.interpolation), nil
+}
---
+++ builder_test.go
@@ -0,0 +1,174 @@
+package colorgrad
+
+import (
+	"image/color"
+	"math"
+	"testing"
+)
+
+func TestBasic1(t *testing.T) {
+	// Single color
+	grad, _ := NewGradient().
+		Colors(color.RGBA{0, 255, 0, 255}).
+		Build()
+	dmin, dmax := grad.Domain()
+	if dmin != 0 || dmax != 1 {
+		t.Errorf("Domain got: (%v, %v), expected: (0, 1)", dmin, dmax)
+	}
+	testStr(t, grad.At(0).Hex(), "#00ff00")
+	testStr(t, grad.At(1).Hex(), "#00ff00")
+
+	// Named colors
+	grad, _ = NewGradient().
+		HtmlColors("tomato", "skyblue", "gold", "springgreen").
+		Build()
+	colors := grad.ColorfulColors(4)
+	testStr(t, colors[0].Hex(), "#ff6347")
+	testStr(t, colors[1].Hex(), "#87ceeb")
+	testStr(t, colors[2].Hex(), "#ffd700")
+	testStr(t, colors[3].Hex(), "#00ff7f")
+
+	// Blend mode
+	grad, _ = NewGradient().
+		HtmlColors("#333", "#bbb").
+		Mode(BlendHcl).
+		Build()
+	testStr(t, grad.At(0).Hex(), "#333333")
+	testStr(t, grad.At(1).Hex(), "#bbbbbb")
+
+	grad, _ = NewGradient().
+		HtmlColors("#333", "#bbb").
+		Mode(BlendHsv).
+		Build()
+	testStr(t, grad.At(0).Hex(), "#333333")
+	testStr(t, grad.At(1).Hex(), "#bbbbbb")
+
+	grad, _ = NewGradient().
+		HtmlColors("#333", "#bbb").
+		Mode(BlendLab).
+		Build()
+	testStr(t, grad.At(0).Hex(), "#333333")
+	testStr(t, grad.At(1).Hex(), "#bbbbbb")
+
+	grad, _ = NewGradient().
+		HtmlColors("#333", "#bbb").
+		Mode(BlendLinearRgb).
+		Build()
+	testStr(t, grad.At(0).Hex(), "#333333")
+	testStr(t, grad.At(1).Hex(), "#bbbbbb")
+
+	grad, _ = NewGradient().
+		HtmlColors("#333", "#bbb").
+		Mode(BlendLuv).
+		Build()
+	testStr(t, grad.At(0).Hex(), "#333333")
+	testStr(t, grad.At(1).Hex(), "#bbbbbb")
+
+	grad, _ = NewGradient().
+		HtmlColors("#333", "#bbb").
+		Mode(BlendRgb).
+		Build()
+	testStr(t, grad.At(0).Hex(), "#333333")
+	testStr(t, grad.At(1).Hex(), "#bbbbbb")
+
+	grad, _ = NewGradient().
+		HtmlColors("#333", "#bbb").
+		Mode(BlendOklab).
+		Build()
+	testStr(t, grad.At(0).Hex(), "#333333")
+	testStr(t, grad.At(1).Hex(), "#bbbbbb")
+}
+
+func TestBasic2(t *testing.T) {
+	// Custom gradient default
+	grad, _ := NewGradient().Build()
+	colors := grad.ColorfulColors(2)
+
+	if len(colors) != 2 {
+		t.Errorf("Expected 2, got %v", len(colors))
+	}
+	testStr(t, colors[0].Hex(), "#000000")
+	testStr(t, colors[1].Hex(), "#ffffff")
+
+	testStr(t, grad.At(math.NaN()).Hex(), "#000000")
+
+	// Custom colors
+	grad, _ = NewGradient().
+		Colors(
+			color.RGBA{255, 0, 0, 255},
+			color.RGBA{255, 255, 0, 255},
+			color.RGBA{0, 0, 255, 255},
+		).
+		Build()
+	colors = grad.ColorfulColors(3)
+
+	if len(colors) != 3 {
+		t.Errorf("Expected 3, got %v", len(colors))
+	}
+	testStr(t, colors[0].Hex(), "#ff0000")
+	testStr(t, colors[1].Hex(), "#ffff00")
+	testStr(t, colors[2].Hex(), "#0000ff")
+
+	testStr(t, grad.At(math.NaN()).Hex(), "#000000")
+
+	// Custom colors #2
+	grad, _ = NewGradient().
+		HtmlColors("#00f", "#00ffff").
+		Colors(color.RGBA{255, 255, 0, 255}).
+		HtmlColors("lime").
+		Build()
+	colors = grad.ColorfulColors(4)
+	testStr(t, colors[0].Hex(), "#0000ff")
+	testStr(t, colors[1].Hex(), "#00ffff")
+	testStr(t, colors[2].Hex(), "#ffff00")
+	testStr(t, colors[3].Hex(), "#00ff00")
+}
+
+func TestError(t *testing.T) {
+	// Invalid HTML colors
+	grad, err := NewGradient().
+		HtmlColors("#777", "bloodred", "#bbb", "#zzz").
+		Build()
+	if err == nil {
+		t.Errorf("It should error")
+	}
+	if !isZeroGradient(grad) {
+		t.Errorf("It should zeroGradient")
+	}
+
+	// Wrong domain 1
+	grad, err = NewGradient().
+		HtmlColors("#777", "#fff", "#ccc", "#222").
+		Domain(0, 0.5, 1).
+		Build()
+	if err == nil {
+		t.Errorf("It should error")
+	}
+	if !isZeroGradient(grad) {
+		t.Errorf("It should zeroGradient")
+	}
+
+	// Wrong domain 2
+	grad, err = NewGradient().
+		HtmlColors("#777", "#fff", "#ccc", "#222").
+		Domain(0, 0.71, 0.70, 1).
+		Build()
+	if err == nil {
+		t.Errorf("It should error")
+	}
+	if !isZeroGradient(grad) {
+		t.Errorf("It should zeroGradient")
+	}
+
+	// Wrong domain 3
+	grad, err = NewGradient().
+		HtmlColors("#777", "#fff", "#ccc", "#222").
+		Domain(1, 0).
+		Build()
+	if err == nil {
+		t.Errorf("It should error")
+	}
+	if !isZeroGradient(grad) {
+		t.Errorf("It should zeroGradient")
+	}
+}
